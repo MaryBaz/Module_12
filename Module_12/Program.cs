21f4dc91590229d84804aa47898ce0836f6129b4
Name <email@example.ru>
2024-08-28 20:41:58 +0500

    Добавьте файлы проекта.

---
+++ Module_12/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Module_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> _users = new List<User>();
+
+            _users.Add(new User("PJ", "Patic", false));
+            _users.Add(new User("TL", "Lisbon", true));
+            _users.Add(new User("SN", "Sami", false));
+
+            Process(_users);
+        }
+        static void Process(List<User> users)
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine($"Hello {user.Name}!");
+                if (!user.IsPremium)
+                    ShowAds();
+            }
+
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+
+    }
+
+}
---
+++ Module_12/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_12
+{
+   
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool isPremium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+        }
+    }
+}
